682808a0ee69f83023dba58dec80af52a0566456
Константин <menace34@bk.ru>
2024-04-27 01:50:05 +0300

    Start! "Hello, I'm bot!"

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os
 
-from pyrogram import Client, filters
+from pyrogram import Client
 
-app = Client("test_task_bot", bot_token=os.getenv("TOKEN", ""))
+api_id = os.getenv("API_ID", "")
+api_hash = os.getenv("API_HASH", "")
+bot_token = os.getenv("TOKEN", "")
+
+app = Client(
+    "test_task_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token
+)
 
 
-@app.on_message(filters.private)
+@app.on_message()
 async def hello(client, message) -> None:
-    await message.reply("Hello from Pyrogram!")
+    await message.reply("Привет, я бот!")
     return
-
 
 app.run()
